ca32cdbdc5d591461286431ab583d9eba6aabc63
TestingDocs.com <support@testingdocs.com>
2020-08-24 15:08:40 +0530

    Demo pull request commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		
 		System.out.println("Code change on Git Demo Branch.");
+		System.out.println("This is to demo pull request.");
 	}
 
 }
